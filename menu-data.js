70761ce90f9877fb39380c61ad3966907e658419
AnnaCavalli <annacavalli6@gmail.com>
2026-06-22 21:14:32 +0200

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -15,7 +15,7 @@ const MENU = {
             },
             description: {
               it: "Tuorlo e senape al miele",
-              en: "Egg yolk and honey mustards",
+              en: "Egg yolk and honey mustard",
             },
             price: "16.00",
             allergens: [3, 10],
